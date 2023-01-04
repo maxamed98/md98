e740f7bfa3e55f4559dead938125c99db3464c52
Mohamed <2108323@brunel.ac.uk>
2023-01-04 17:14:26 +0000

    added il-v2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import illibero from "./images/il-libero.jpg";
 import ttt from "./images/ttt.jpg";
 import calculator from "./images/calculator.jpg";
 import charter from "./images/charter.jpg";
+import il_v2 from "./images/il-v2.jpg";
 
 function App() {
   const apps = [
@@ -13,6 +14,8 @@ function App() {
       url: "http://il-libero.com",
       desc: "This site can be used to watch any movie, tv show or anime ever made",
       image: illibero,
+      languages: ["javascript"],
+      frameworks: ["react"],
       date: "06/04/22",
     },
     {
@@ -22,6 +25,8 @@ function App() {
       url: "http://md98-ttt.netlify.app",
       desc: "N/A",
       image: ttt,
+      languages: ["javascript"],
+      frameworks: ["react"],
       date: "16/07/22",
     },
     {
@@ -31,6 +36,8 @@ function App() {
       url: "http://md98-calculator.netlify.app",
       desc: "N/A",
       image: calculator,
+      languages: ["javascript"],
+      frameworks: ["react"],
       date: "17/07/22",
     },
     {
@@ -40,7 +47,20 @@ function App() {
       url: "http://md98-charter.netlify.app",
       desc: "This tool creates a chart when given input data",
       image: charter,
+      languages: ["javascript"],
+      frameworks: ["react"],
       date: "20/07/22",
+    },
+    {
+      name: "Il-v2",
+      type: "Streaming App",
+      level: "Intermediate",
+      url: "http://il-v2.com",
+      desc: "This site can be used to watch any movie, tv show or anime ever made",
+      image: il_v2,
+      languages: ["javascript"],
+      frameworks: ["react"],
+      date: "01/10/22",
     },
   ];
 
@@ -53,7 +73,7 @@ function App() {
         <h2>Projects:</h2>
         <div className="apps-cont">
           {apps.map((app) => (
-            <a href={app.url} target="_blank">
+            <a href={app.url} target="_blank" key={app.url}>
               <div className="app">
                 <div className="title">
                   <h4>{app.name}</h4>
@@ -68,6 +88,12 @@ function App() {
                   </h5>
                   <h5>
                     <span>Description:</span> {app.desc}
+                  </h5>
+                  <h5>
+                    <span>Languages:</span> {app.languages}
+                  </h5>
+                  <h5>
+                    <span>Frameworks:</span> {app.frameworks}
                   </h5>
                   <h5>
                     <span>Date:</span> {app.date}
